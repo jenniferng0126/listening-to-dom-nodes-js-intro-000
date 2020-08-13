69c37f7662083a932ce8609bf31e2c61ad58080b
Jennifer Ng <ka.man.jennifer.ng@gmail.com>
2020-08-13 23:37:53 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -5,3 +5,12 @@ main.addEventListener('click', function(event){alert('I was clicked!')});
 
 const input = document.querySelector('input');
 input.addEventListener('keydown', function(e){console.log(e.key)});
+
+const input = document.querySelector('input');
+input.addEventListener('keydown', function(e) {
+  if (e.key === 'g') {
+    e.preventDefault;
+  } else {
+    console.log(e.key);
+  }
+})
